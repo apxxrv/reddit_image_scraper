29534d0650b84d44c8a8fefa58c0804ea2f89231
apxxrv <108655991+apxxrv@users.noreply.github.com>
2024-01-13 14:06:35 -0700

    Update SubDownloadList.py

--- SubDownloadList.py
+++ SubDownloadList.py
@@ -7,11 +7,11 @@ from dotenv import load_dotenv
 
 # Load environment variables for Reddit API credentials
 load_dotenv()
-client_id = os.getenv("ZAGz-tAWTZlqijUKyVjnMw")
-client_secret = os.getenv("x4M2wUIG7r1wi7qDA-_kuIXobyU5jg")
-user_agent = os.getenv("reddit_image_scraper by u/Ambitious-Street-516")
-username = os.getenv("u/Ambitious-Street-516")
-password = os.getenv("@poorvS1ngh")
+client_id = os.getenv("XXXXXXX")
+client_secret = os.getenv("XXXXXXX")
+user_agent = os.getenv("reddit_image_scraper by username")
+username = os.getenv("XXXXXXX")
+password = os.getenv("XXXXXXX")
 
 # Initialize PRAW with credentials
 reddit = praw.Reddit(client_id=client_id,
